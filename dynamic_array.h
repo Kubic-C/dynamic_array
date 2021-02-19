60c9d4635ed58ee43de543982c8efbd26b93a8e8
Kubic <56777409+Kubic-C@users.noreply.github.com>
2021-02-19 07:41:08 -0600

    fixed issue with file decor at top

--- dynamic_array.h
+++ dynamic_array.h
@@ -1,19 +1,19 @@
 /*
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- *																	*
- *				Copyright 2021 Sawyer Porter						*
- *																    *
+ *								    *
+ *				Copyright 2021 Sawyer Porter	    *
+ *								    *
  * Licensed under the Apache License, Version 2.0 (the "License");  *
  * you may not use this file except in compliance with the License. *
- * You may obtain a copy of the License at							*
- * http://www.apache.org/licenses/LICENSE-2.0						*
- *																	*
+ * You may obtain a copy of the License at			    *
+ * http://www.apache.org/licenses/LICENSE-2.0			    *
+ *							            *
  * Unless required by applicable law or agreed to in writing,       *
  * software distributed under the License is distributed on an      *
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,     *
  * either express or implied.See the License for the specific       *
  * language governing permissionsand limitations under the License. *
- *																	*
+ *								    *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
  * 
  * // vvv READ THIS vvv //
@@ -30,11 +30,9 @@
  * intercompatible with std::vector do:
  * #define DYNM_ARR_VECTOR_COMPATIBLE
  *
- * 
  * if you want to define to change 
  * the namespace name, do:
  * #define DYNM_ARR_NAMESPACE_NAME [enter the namespace name here]
- * 
  * by defualt it is "kubic"
  * 
  * you should use the std::vector<> class
--- testing/main.cpp
+++ testing/main.cpp
@@ -1,36 +1,7 @@
-
-#define DYNM_ARR_VECTOR_COMPATIBLE
 #include <dynamic_array.h>
-
-#include <iostream>
-#include <vector>
-#include <string>
 
 int main()
 {
 	kubic::dynamic_array<int> dynm_arr;
-	dynm_arr.push_back(0);
-	dynm_arr.push_back(1);
-	dynm_arr.push_back(2);
-	dynm_arr.push_back(3);
-
-	std::vector<int> vector = dynm_arr;
-	dynm_arr = kubic::convert_to_dynm_arr(vector);
-	std::vector<int> test = { 0, 1, 2, 3 };
-
-	dynm_arr.insert(dynm_arr.end(), test.begin(), test.end());
-	
-	dynm_arr = { 5, 1, 2 };
-
-	std::cout 
-		<< "vector: \n"
-		<< "vector capacity: " << dynm_arr.capacity() << '\n'
-		<< "vector size: " << dynm_arr.size() << '\n';
-
-	for (int& i : dynm_arr)
-		std::cout << i << '\n';
-
-
-	std::cin.get();
 	return 0;
 }
